5d1f165fb8c121104136b2fb5dd5f98c9ab3b3bc
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-03-09 18:35:42 +0100

    Aanmaken Project in VS 2013

---
+++ finah-backend/Finah_Backend/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Finah_Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ finah-backend/Finah_Backend/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Finah_Backend.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
