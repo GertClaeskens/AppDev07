22accd428f55a6b6fcff8dd28fabe1c6e45a78dd
brian thys <brianthys@hotmail.com>
2015-03-17 20:57:02 +0100

    Update
    Update voor genereren link

--- finah-backend/Finah_Backend/Controllers/VragenController.cs
+++ finah-backend/Finah_Backend/Controllers/VragenController.cs
@@ -11,14 +11,26 @@ namespace Finah_Backend.Controllers
     public class VragenController : ApiController
     {
         private string sourceUrl = "Http://www.ExampleSite.be/Vragen/";
+        private string link;
         // GET api/vragen/5
         [Route("Vragen/{id}")] //Geen Api/ meer nodig
         public string Get(String id) // return -> naderhand veranderen in Bevraging
         {
+            string testLink;
             //return "Ingegeven ID: " + id;
             //vragen ophalen en antwoorden linken aan persoon
             //Test genereren Unique ID (Source = http://stackoverflow.com/questions/11313205/generate-a-unique-id)
-            return string.Format("{0}{1:N}", sourceUrl, Guid.NewGuid());
+            link = string.Format("{0}{1:N}", sourceUrl, Guid.NewGuid());
+            for (int i = 0; i < 1000; i++) //alle waardes ophalen uit DB en controleren op duplicate (word foreach)
+            {
+                testLink = string.Format("{0}{1:N}", sourceUrl, Guid.NewGuid()); //testlink genereren om te controleren;
+                if (testLink.Equals(link)) //Controle op duplicate
+                {
+                    link = string.Format("{0}{1:N}", sourceUrl, Guid.NewGuid()); //nieuwe link genereren
+                    i = 0; //restart loop om nieuwe link te controleren
+                }
+            }
+                return link; //Momenteel gegenereerde link tonen
         }
 
         // GET api/vragen/Overzicht
