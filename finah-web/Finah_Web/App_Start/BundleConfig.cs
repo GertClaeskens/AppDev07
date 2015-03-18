7755c47cb774a42b481615ddac100b5d47cb041b
Rsarrechia <rafael.sarrechia@student.pxl.be>
2015-03-18 17:23:25 +0100

    Bevraging-overzicht layout(voorlopig)

--- finah-web/Finah_Web/App_Start/BundleConfig.cs
+++ finah-web/Finah_Web/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace Finah_Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Stylesheet.css"));
         }
     }
 }
