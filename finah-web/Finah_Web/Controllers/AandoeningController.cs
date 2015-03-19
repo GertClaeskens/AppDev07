9cd39242fcd265c22ca6386ffe82fee72a5ad427
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-03-19 17:59:49 +0100

    Aandoeningcontroller aangemaakt in web applicatie

---
+++ finah-web/Finah_Web/Controllers/AandoeningController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finah_Web.Controllers
+{
+    public class AandoeningController : Controller
+    {
+        // GET: Aandoening
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Aandoening/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Aandoening/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Aandoening/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aandoening/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Aandoening/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Aandoening/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Aandoening/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
