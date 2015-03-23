83f6126bfc56dbe2d7326823f5bca7b1496b487b
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-03-23 23:05:10 +0100

    [Desktop] Code AandoeningDAO vereenvoudigd

--- finah-desktop/DAO/finah_desktop/SharedDAO.java
+++ finah-desktop/DAO/finah_desktop/SharedDAO.java
@@ -3,12 +3,17 @@ package finah_desktop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class SharedDAO {
 
@@ -24,5 +29,19 @@ public class SharedDAO {
 				.getEntity().getContent()));
 
 		return rd;
+	}
+	public static <T> ArrayList<T> HaalGegevens(String url,Type typ)
+			throws ClientProtocolException, IOException {
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+		// HttpGet request = new
+		// HttpGet("http://finahbackend1920.azurewebsites.net/Aandoening/Overzicht");
+		HttpResponse response = client.execute(request);
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response
+				.getEntity().getContent()));
+
+		ArrayList<T> lijst = gson.fromJson(rd, typ);
+		return lijst;
 	}
 }
