f5a4335d29ef8fa26278cb10ed20be6890b2510a
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-03-25 21:23:14 +0100

    [WEB] Testprogramma om objecten uit te lezen op web API

---
+++ finah-web/Probeersels/jsonTest.php
@@ -0,0 +1,16 @@
+<?php
+// request list of contacts from Web API
+$json = file_get_contents('http://localhost:1695/Aandoening/Overzicht');
+// deserialize data from JSON
+$aandoening = json_decode($json);
+for ($a = 0; $a < count($aandoening); $a++) {
+    echo $aandoening[$a]->Id . "\n";
+    echo $aandoening[$a]->Omschrijving . "\n";
+    for ($x = 0; $x < count($aandoening[$a]->Patologieen); $x++) {
+        echo $aandoening[$a]->Patologieen[$x]->Id . "\n";
+        echo $aandoening[$a]->Patologieen[$x]->Omschrijving . "\n";
+    }
+}
+
+
+?> 
