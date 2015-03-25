6b5b5296ad37a53f7d499ab625f00aa80c17281f
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-03-25 09:38:22 +0100

    [Web]  Antwoord Controller en AntwoordenlijstController
    aangemaakt
    [Backend] Property hernoemt in Antwoord

--- finah-web/Finah_Web/Models/Antwoord.cs
+++ finah-web/Finah_Web/Models/Antwoord.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Finah_Backend.Models
+namespace Finah_Web.Models
 {
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Dynamic;
-
     public class Antwoord
     {
         public int Id { get; set; }
         public int Antword { get; set; }
-
-
     }
 }
