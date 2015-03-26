7b90ccb9de4ce249da2e18be09fec86e1e7824e8
Rsarrechia <rafael.sarrechia@student.pxl.be>
2015-03-26 11:26:24 +0100

    [WEB] Layout overzichten

--- finah-web/Probeersels/aandoeningOverzicht.php
+++ finah-web/Probeersels/aandoeningOverzicht.php
@@ -3,6 +3,7 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="Stylesheet.css"/>
+</head>
 <body>
 <div id="wrapper">
     <div id="page-header">
@@ -11,18 +12,12 @@
     <div id="inner-wrapper">
         <div id="nav-bar2">
             <h2> Beheren </h2>
-            <button onclick="location.href='#'">Aandoening </button>
-            <button onclick="location.href='#'">Pathologie </button>
-
-            <button onclick="location.href='#'">Leeftijdscategorie </button>
-
-            <button onclick="location.href='#'">Vragen </button>
-
-            <button onclick="location.href='#'">Vragenlijsten</button>
-            <button onclick="location.href='#'">Terug naar home </button>
-
-
-
+            <button onclick="location.href='aandoeningOverzicht.php'">Aandoening </button>
+            <button onclick="location.href='PathologieOverzicht.php'">Pathologie </button>
+            <button onclick="location.href='LeeftijdsCategorieOverzicht.php'">Leeftijdscategorie </button>
+            <button onclick="location.href='VragenOverzicht.php'">Vragen </button>
+            <button onclick="location.href='VragenlijstOverzicht.php'">Vragenlijsten</button>
+            <button onclick="location.href='index.php'">Terug naar home </button>
 
         </div><!--Closing DIV nav-bar-->
         <div id="body-container">
@@ -42,9 +37,13 @@
                     </th>
                     <th >Actie</th>
                 </tr>
-                <!-- <?php foreach($aandoeningLijst as $item)  {echo "<tr> <td> $item</td>      <td class='action-column'>
-                            <a href='edit.php'>Edit</a> | <a href='delete.php'>Delete</a> | <a href='details.php'>Details</a>
-                        </td></tr>";} ?>-->
+                <!-- <?php foreach($aandoeningLijst as $item)
+                {echo "<tr>
+                            <td> $item</td>
+                             <td class='action-column'>
+                                <a href='edit.php'>Edit</a> | <a href='delete.php'>Delete</a> | <a href='details.php'>Details</a>
+                         </td>
+                         </tr>";} ?>-->
                 <tr>
                     <td class="large-column">
                         Dit is een voorbeeld omschrijving.
@@ -104,10 +103,7 @@
 
                     </td>
                 </tr>
-
-
             </table>
-
         </div><!--Closing DIV body containerr-->
     </div><!--Closing DIV innerwrapper-->
     <div id="page-footer">
@@ -115,5 +111,4 @@
     </div>
 </div><!--Closing DIV wrapper-->
 </body>
-</head>
 </html>
