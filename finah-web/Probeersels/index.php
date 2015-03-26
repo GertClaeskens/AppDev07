e54dc471ce35b15d78d764445add885dd984e39c
Rsarrechia <rafael.sarrechia@student.pxl.be>
2015-03-26 11:49:20 +0100

    [WEB] Layout Index

--- finah-web/Probeersels/index.php
+++ finah-web/Probeersels/index.php
@@ -1,111 +1,53 @@
 <?php
 
 ?>
+<!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>FINAH - Home</title>
     <link rel="stylesheet" type="text/css" href="Stylesheet.css"/>
+</head>
 <body>
 <div id="wrapper">
     <div id="page-header">
+
         <h1>FINAH</h1>
+
     </div> <!--Closing DIV page header-->
     <div id="inner-wrapper">
-        <div id="nav-bar1">
-            <h2> Beheren </h2>
-            <button onclick="location.href='aandoeningOverzicht.php'">Aandoening </button>
-            <button onclick="location.href='PathologieOverzicht.php'">Pathologie </button>
-            <button onclick="location.href='LeeftijdsCategorieOverzicht.php'">Leeftijdscategorie </button>
-            <button onclick="location.href='VragenOverzicht.php'">Vragen </button>
-            <button onclick="location.href='VragenlijstOverzicht.php'">Vragenlijsten</button>
-            <button onclick="location.href='index.php'">Terug naar home </button>
-
+        <div id="nav-bar">
+            <h2> Menu </h2>
+            <button onclick="location.href='index.php'">Home </button>
+            <button onclick="location.href='Account.php'">Mijn account </button>
+            <button onclick="location.href='BevragingOverzicht.php'">Bevragingen</button>
+            <button onclick="location.href='aandoeningOverzicht.php'">Beheren </button>
+            <button onclick="location.href='#'">Uitloggen</button>
         </div><!--Closing DIV nav-bar-->
         <div id="body-container">
             <h3 id="Breadcrumb">Menu > Home</h3>
-            <h2 id="Content-Title">Home</h2>
-            <p>
-                <a href="create.php">Create new</a>
-            </p>
-            <br />
-            <table border="1" class="overzicht-table">
-                <tr>
-                    <th>
-                        Van
-                    </th>
-                    <th>
-                        Tot
-                    </th>
-                    <th >Actie</th>
-                </tr>
-                <!-- <?php foreach($leeftijdCategorieLijst as $item)
-                {echo "<tr>
-                            <td> $item</td>
-                             <td class='action-column'>
-                                <a href='edit.php'>Edit</a> | <a href='delete.php'>Delete</a> | <a href='details.php'>Details</a>
-                         </td>
-                         </tr>";} ?>-->
-                <tr>
-                    <td class="medium-column">
-                        0
-                    </td>
-                    <td class="medium-column">
-                        1
-                    </td>
-                    <td class="action-column">
-                        <a href="edit.php">Edit</a> | <a href="delete.php">Delete</a> | <a href="details.php">Details</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="medium-column">
-                        0
-                    </td>
-                    <td class="medium-column">
-                        1
-                    </td>
-                    <td class="action-column">
-                        <a href="edit.php">Edit</a> | <a href="delete.php">Delete</a> | <a href="details.php">Details</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="medium-column">
-                        0
-                    </td>
-                    <td class="medium-column">
-                        1
-                    </td>
-                    <td class="action-column">
-                        <a href="edit.php">Edit</a> | <a href="delete.php">Delete</a> | <a href="details.php">Details</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="medium-column">
-                        0
-                    </td>
-                    <td class="medium-column">
-                        1
-                    </td>
-                    <td class="action-column">
-                        <a href="edit.php">Edit</a> | <a href="delete.php">Delete</a> | <a href="details.php">Details</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="medium-column">
-                        0
-                    </td>
-                    <td class="medium-column">
-                        1
-                    </td>
-                    <td class="action-column">
-                        <a href="edit.php">Edit</a> | <a href="delete.php">Delete</a> | <a href="details.php">Details</a>
-                    </td>
-                </tr>
-            </table>
+            <h2 id="Content-Title"> Home </h2>
+            <article>
+
+
+                Quisque sed lorem blandit, pretium odio non, volutpat erat. Nam eros tortor, hendrerit laoreet accumsan ut, commodo sed diam.
+                Praesent non sodales quam. Vestibulum ante leo, rutrum nec malesuada eget, accumsan et justo. Duis a orci et dolor tempus
+                rutrum at sit amet mauris. Vivamus consectetur libero vel metus iaculis, ut facilisis arcu rhoncus. Morbi ut nulla non sem malesuada pretium.
+                <br />
+                <br />
+                Quisque sed lorem blandit, pretium odio non, volutpat erat. Nam eros tortor, hendrerit laoreet accumsan ut, commodo sed diam.
+                Praesent non sodales quam. Vestibulum ante leo, rutrum nec malesuada eget, accumsan et justo. Duis a orci et dolor tempus
+                rutrum at sit amet mauris. Vivamus consectetur libero vel metus iaculis, ut facilisis arcu rhoncus. Morbi ut nulla non sem malesuada pretium.
+                <br />
+                <br />
+            </article>
         </div><!--Closing DIV body containerr-->
     </div><!--Closing DIV innerwrapper-->
     <div id="page-footer">
         <p>&copy; Copyright 2015-2016. All Rights Reserved</p>
     </div>
 </div><!--Closing DIV wrapper-->
+
 </body>
-</head>
 </html>
