608d9a3b3822a2d88eb570289de31073d17d72ed
brian thys <brianthys@hotmail.com>
2015-04-01 11:41:33 +0200

    [WEB] vraaginvullen
    vraag invullen

---
+++ finah-web/Probeersels/VraagInvuller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brian
+ * Date: 1/04/2015
+ * Time: 10:20
+ */
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Stylesheet.css"/>
+    <title>FINAH - Bevraging</title>
+<body>
+<form method="POST" <?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>>
+    //buttons keuze + texts n shit
+</form>
+</body>
+</head>
+</html>
