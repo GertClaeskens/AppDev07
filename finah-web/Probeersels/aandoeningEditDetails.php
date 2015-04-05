76995cccc7ef193a14dcac6a0137d55ddeac0a77
Gert Claeskens <gert.claeskens@student.pxl.be>
2015-04-05 14:50:21 +0200

    [Web] AandoeningEditDetails afgewerkt voor de huidige velden

--- finah-web/Probeersels/aandoeningEditDetails.php
+++ finah-web/Probeersels/aandoeningEditDetails.php
@@ -6,8 +6,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width"/>
     <title>FINAH - Aandoening</title>
     <link rel="stylesheet" type="text/css" href="Stylesheet.css"/>
 </head>
@@ -15,7 +15,8 @@
 <div id="wrapper">
     <div id="page-header">
         <h1>FINAH</h1>
-    </div> <!--Closing DIV page header-->
+    </div>
+    <!--Closing DIV page header-->
     <div id="inner-wrapper">
         <div id="nav-bar2">
             <h2> Beheren </h2>
@@ -25,17 +26,33 @@
             <button onclick="location.href='VragenOverzicht.php'">Vragen</button>
             <button onclick="location.href='VragenlijstOverzicht.php'">Vragenlijsten</button>
             <button onclick="location.href='index.php'">Terug naar home</button>
-        </div><!--Closing DIV nav-bar-->
+        </div>
+        <!--Closing DIV nav-bar-->
         <div id="body-container">
-            <h3 id="Breadcrumb">Menu > Aandoening > Bewerken</h3>
-            <h2 id="Content-Title">Aandoening bewerken</h2>
-            <hr/>
-
             <?php
+                if (isset($_POST)) {
+                    $aandoening = FinahDAO::HaalOp("Aandoening", $_POST["Id"]);
+                    $naam = $aandoening["Omschrijving"];
                     if (isset($_POST["bewerk"])) {
 
-                            $aandoening = FinahDAO::HaalOp("Aandoening",$_POST["Id"]);
-                        var_dump($aandoening["Omschrijving"]);
+                        echo "<h3 id = 'Breadcrumb' > Menu > Aandoening > Bewerken</h3 >";
+                        echo "<h2 id = 'Content-Title' > Bewerken : ". $naam . "  </h2 >";
+                    } elseif (isset($_POST["details"])) {
+                        echo "<h3 id = 'Breadcrumb' > Menu > Aandoening > Details</h3 >";
+                        echo "<h2 id = 'Content-Title' > Details : ".$naam." </h2 >";
+                    }
+                }
+
+            ?>
+            <hr/>
+            <form method="POST">
+                <ul class="form-style">
+                    <li><label class="control-label">Omschrijving</label></li>
+                    <?php
+                        if (isset($_POST["bewerk"])) {
+
+
+                        //var_dump($aandoening["Omschrijving"]);
                         /*               foreach($_POST as $key => $value) {
                                            $pos = strpos($key , "edit_");
                                            if ($pos === 0){
@@ -43,47 +60,76 @@
                                            }
                                        }*/
                         ?>
-                        <form method="POST">
-                            <ul class="form-style">
-                                <li><label class="control-label">Omschrijving</label></li>
-                                <li><input class="form-control" type="text" name="omschrijving" value=<?php echo $aandoening["Omschrijving"]; ?>/></li>
-                                <li><label class="control-label">Kies een pathologie</label></li>
-                                <select class="form-control" name="pathologie[]" multiple="multiple">
-                                    <!--                        Pathologieen ophalen-->
-                                    <?php
-                                        //$patologieen = new PathologieArray();
-                                        //TODO omzetten naar Pathologie object
-                                        $patologieen = FinahDAO::HaalOp("Pathologie");
-                                        foreach ($patologieen as $item) {
 
-                                            echo "<option value='".$item["Id"]."'>" . $item["Omschrijving"] . "</option>\r\n";
-                                        }
-                                        //var_dump($patologieen);
-                                        //                        for ($a=0;$a<count($patologieen);$a++){
-                                        //                            echo "<option>" . $patologieen->Omschrijving . "</option>\r\n";
-                                        //                        }
-                                    ?>
+                        <li><input class="form-control" type="text" name="omschrijving"
+                                   value=<?php echo $aandoening["Omschrijving"]; ?>/></li>
+                        <li><label class="control-label">Kies een pathologie</label></li>
+                        <select class="form-control" name="pathologie[]" multiple="multiple">
+                            <!--                        Pathologieen ophalen-->
+                            <?php
+                                //$patologieen = new PathologieArray();
+                                //TODO omzetten naar Pathologie object
+                                $patologieen = FinahDAO::HaalOp("Pathologie");
+                                foreach ($patologieen as $item) {
 
-                                </select>
+                                    echo "<option value='" . $item["Id"] . "'>" . $item["Omschrijving"] . "</option>\r\n";
+                                }
+                                //var_dump($patologieen);
+                                //                        for ($a=0;$a<count($patologieen);$a++){
+                                //                            echo "<option>" . $patologieen->Omschrijving . "</option>\r\n";
+                                //                        }
 
-                                <li><input type="submit" value="Edit" class="createBtn" name="creeer"/></li>
-                            </ul>
-                        </form>
+                                //TODO Opslaan gegevens van Edit implementeren
+                                //TODO gegevens tonen van Geluidsfragment
+                                //TODO gegevens tonen van Afbeelding
+                            ?>
+
+                        </select>
+
+                        <li>                        <button class="actieBtn" onclick="window.location='aandoeningOverzicht.php';return false;">
+                                Terug
+                            </button>
+                            <input type="submit" value="Edit" class="createBtn" name="creeer"/></li>
+
                     <?php
-                    }elseif (isset($_POST["details"])){
-                        ?>
-                        <div class="Back">
-                            <a href="aandoeningOverzicht.php">Terug naar overzicht</a>
-                        </div>
+                    }elseif (isset($_POST["details"]))
+                        {
+                    ?>
+
+                    <li><?php echo $aandoening["Omschrijving"]; ?></li>
+                    <li><label class="control-label">Bijhorende Pathologieën</label></li>
                     <?php
-                    };
+                        foreach ($aandoening["Patologieen"] as $pat) {
+                            echo "<li> " . $pat["Omschrijving"] . "</li>";
+                        }
+                    ?>
+                    <li>
+                        <button class="actieBtn" onclick="window.location='aandoeningOverzicht.php';return false;">
+                            Terug
+                        </button>
+                    </li>
+
+
+                </ul>
+            </form>
+        <?php
+            } elseif (isset($_POST["details"])) {
             ?>
-        </div><!--Closing DIV body containerr-->
-    </div><!--Closing DIV innerwrapper-->
+            <div class="Back">
+                <a href="aandoeningOverzicht.php">Terug naar overzicht</a>
+            </div>
+        <?php
+        };
+        ?>
+        </div>
+        <!--Closing DIV body containerr-->
+    </div>
+    <!--Closing DIV innerwrapper-->
     <div id="page-footer">
         <p>&copy; Copyright 2015-2016. All Rights Reserved</p>
     </div>
-</div><!--Closing DIV wrapper-->
+</div>
+<!--Closing DIV wrapper-->
 
 </body>
 </html>
