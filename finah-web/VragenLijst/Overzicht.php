cf762e94274c9ca92547377d3b389821551483b9
Rsarrechia <rafael.sarrechia@student.pxl.be>
2015-04-27 22:54:36 +0200

    [WEB]    LInk aangepast

--- finah-web/VragenLijst/Overzicht.php
+++ finah-web/VragenLijst/Overzicht.php
@@ -77,7 +77,7 @@ require "../PHP/DAO/FinahDAO.php";
                 <a href="../Pathologie/Overzicht.php"> Pathologie</a>
             </li>
             <li>
-                <a href="../LeeftijdsCategorie/Overzich.php"> Leeftijdscategorie</a>
+                <a href="../LeeftijdsCategorie/Overzicht.php"> Leeftijdscategorie</a>
             </li>
             <li>
                 <a href="../Vragen/Overzicht.php"> Vragen</a>
