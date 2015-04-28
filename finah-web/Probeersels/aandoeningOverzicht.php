c30403b1c964da1700bcfc8873e4e99862aa5b83
Rsarrechia <rafael.sarrechia@student.pxl.be>
2015-04-28 22:09:47 +0200

    [WEB]   Create pagina aandoening

--- finah-web/Probeersels/aandoeningOverzicht.php
+++ finah-web/Probeersels/aandoeningOverzicht.php
@@ -1,53 +1,53 @@
 <?php
 require "../PHP/DAO/FinahDAO.php";
-require_once "../PHP/Models/Bevraging.php";
+require "../PHP/Models/Aandoening.php";
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>FINAH - Bevraging</title>
-    <link rel="stylesheet" type="text/css" href="../Css/stylesheet3.css"/>
-    <link rel="stylesheet" type="text/css" href="../Css/bootstrap.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>FINAH - Aandoening</title>
+        <link rel="stylesheet" type="text/css" href="../Css/stylesheet3.css"/>
+        <link rel="stylesheet" type="text/css" href="../Css/bootstrap.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
 <body>
-<nav  class="navbar navbar-default navbar-fixed-top">
-    <div  class="navbar-header pull-left">
+    <nav  class="navbar navbar-default navbar-fixed-top">
+        <div  class="navbar-header pull-left">
 
-        <a href="#menu-toggle"  id="menu-toggle" class="btn-toggle">
-            <span id="side-toggle" class="glyphicon glyphicon-option-horizontal"></span>
-        </a>
-        <a  class="navbar-brand header"  href="#"> Finah</a>
-    </div>
-    <div class="dropdown navbar-header pull-right nav-right">
-        <span class="img-circle"><img src="../Images/blank-avatar.png"/></span>  <!--TODO  PHP if'ke maken voor als er een avatar/profiel foto beschikbaar is in database of niet ( dan blank-avatar gebruiken) -->
-        <a class="btn dropdown-toggle pull-left" type="button" id="menu1" data-toggle="dropdown">Rafaël.Sarrechia
-            <span class="caret"></span>
-        </a>
-        <ul class="dropdown-menu " role="menu" aria-labelledby="menu1">
-            <li role="presentation">
-                <a role="menuitem" tabindex="0" href="#">
-                    <span class="glyphicon glyphicon-user"></span> &nbsp Mijn account
-                </a>
-            </li>
-            <li role="presentation" class="divider">
-            </li>
-            <li role="presentation">
-                <a role="menuitem" tabindex="-1" href="#">
-                    <span class="glyphicon glyphicon-log-out"></span> &nbsp Uitloggen
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
+            <a href="#menu-toggle"  id="menu-toggle" class="btn-toggle">
+                <span id="side-toggle" class="glyphicon glyphicon-option-horizontal"></span>
+            </a>
+            <a  class="navbar-brand header"  href="#"> Finah</a>
+        </div>
+        <div class="dropdown navbar-header pull-right nav-right">
+            <span class="img-circle"><img src="../Images/blank-avatar.png"/></span>  <!--TODO  PHP if'ke maken voor als er een avatar/profiel foto beschikbaar is in database of niet ( dan blank-avatar gebruiken) -->
+            <a class="btn dropdown-toggle pull-left" type="button" id="menu1" data-toggle="dropdown">Rafaël.Sarrechia
+                <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu " role="menu" aria-labelledby="menu1">
+                <li role="presentation">
+                    <a role="menuitem" tabindex="0" href="#">
+                        <span class="glyphicon glyphicon-user"></span> &nbsp Mijn account
+                    </a>
+                </li>
+                <li role="presentation" class="divider">
+                </li>
+                <li role="presentation">
+                    <a role="menuitem" tabindex="-1" href="#">
+                        <span class="glyphicon glyphicon-log-out"></span> &nbsp Uitloggen
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 <div id="wrapper">
     <!-- Sidebar -->
     <div id="sidebar-wrapper">
@@ -89,80 +89,96 @@ require_once "../PHP/Models/Bevraging.php";
         </ul>
     </div>
     <div  id="page-content-wrapper">
-        <div class="breadcrumb">
-            <a href="../index.php"><span class="glyphicon glyphicon-home"> </a></span> <span class="breadcrumb-font"> &nbsp/ Home / Bevraging </span>        </div>
-        <div  class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12">
-                    <h1>Overzicht</h1>
-                    <button class="btn btn-primary createbtn " onclick="window.location='Create.php';return false;">
-                        Maak een nieuwe bevraging aan
-                    </button>
+    <div class="breadcrumb">
+        <a href="../index.php"><span class="glyphicon glyphicon-home"> </a></span> <span class="breadcrumb-font"> &nbsp/ Home / Aandoening  </span>
+    </div>
+    <div  class="container-fluid">
+    <div class="row">
+    <div class="col-sm-12 col-md-12 col-lg-12">
+    <h1 class="header">Nieuwe aandoening </h1>
+<form class="form-horizontal " role="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <?php
+        if (isset($_POST["creeer"])) {
+            //var_dump($_POST);
+            $omschrijving = $_POST["omschrijving"];
+            $patologielijst = $_POST["pathologie"];
+            //TODO misschien alle objecten van Pathologie ophalen en dan uit die lijst selecteren
+            $aandoening = new Aandoening();
+            $aandoening->setId(0);
+            $aandoening->setOmschrijving($omschrijving);
+            //                    $aandoening->setPatologieen($patologielijst);
+            for ($a = 0; $a < count($patologielijst); $a++) {
+                $aandoening->voegPathologieAanLijstToe(FinahDAO::HaalOp("Pathologie", $patologielijst[$a]));
+            };
+            //var_dump
 
-                    <form action="EditDetails.php" method="post">
-                        <table class="table table-bordered table-responsive table-striped">
-                            <thead>
-                                <tr>
-                                    <th>
-                                        Aangevraagd
-                                    </th>
-                                    <th>
-                                        Informatie
-                                    </th>
-                                    <th>
-                                        Relatie
-                                    </th>
-                                    <th>Actie</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td class="col-sm-1 col-md-1 col-lg-1 text-center">
-                                    1/04/2015
-                                </td>
-                                <td class="col-sm-7 col-md-7 col-lg-7">
-                                    Voorbeeld informatie
-                                </td>
-                                <td class="col-sm-2 col-md-2 col-lg-2 text-center">
-                                    Getrouwd
-                                </td>
-                                        <td class='action-column col-sm-2 col-md-2 col-lg-2'>
-<!--                                        <input type='hidden' name='Id' value=" . $item["Id"] />-->
-                                            <button type='submit' name='details' class='btn btn-primary'>
-                                                <span class='glyphicon glyphicon-list-alt'></span>&nbsp;
-                                            </button>
-                                                  <button type='submit' name='bewerk' class='btn btn-primary'>
-                                                <span class='glyphicon glyphicon-pencil'></span>&nbsp;
-                                            </button>
-                                            <button type='submit'  name='delete' class='btn btn-primary'>
-                                                <span class='glyphicon glyphicon-remove'></span>&nbsp;
-                                            </button>
-                                        <!-- TODO DeleteButton alert window voor bevestiging (JavaScript modal bootstrap hebben we gezien bij .net) -->
-                                   </tr>
-<!--                            ";-->
-<!--                                    }-->
-<!--                                 ?>-->
-                            </tbody>
-                        </table>
-                    </form>
-                </div>
-            </div>
+
+            if (FinahDAO::SchrijfWeg("Aandoening", $aandoening)) {
+                //Todo eventueel een exception toevoegen hier
+                //header("Location: Overzicht.php");
+                echo "De aandoening werd succesvol opgeslagen";
+            }
+            //$aandoening->setPatologieen($patologielijst);
+            //var_dump($aandoening);
+
+}else {
+    ?>
+                    <div class="form-group top-form">
+                        <label class="control-label col-xs-4  col-sm-4 col-md-2 col-lg-2" for="Omschrijving"> Omschrijving: </label>
+                        <div class=" col-xs-8 col-sm-8 col-md-8 col-lg-4">
+                            <textarea rows="5" type="text" class="form-control" id="Omschrijving" placeholder="Voer een omschrijving in"> </textarea>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-xs-4 col-sm-4 col-md-2 col-lg-2" for="Pathologie"> Kies een pathologie:  </label>
+                        <div class="col-xs-6 col-sm-5 col-md-5 col-lg-3">
+                            <select multiple class="form-control" id="Pathologie" name="pathologie[]">
+                                <!--                        Pathologieen ophalen-->
+                                <?php
+                                    //$patologieen = new PathologieArray();
+                                    //TODO omzetten naar Pathologie object
+                                    $patologieen = FinahDAO::HaalOp("Pathologie");
+                                    foreach ($patologieen as $item) {
+                                        $waarde = $item["Omschrijving"];
+                                        echo "<option value='" . $item["Id"] . "'>" . $item["Omschrijving"] . "</option>\r\n";
+                                    }
+                                    //var_dump($patologieen);
+                                    //                        for ($a=0;$a<count($patologieen);$a++){
+                                    //                            echo "<option>" . $patologieen->Omschrijving . "</option>\r\n";
+                                    //                        }
+                                ?>
+
+                        </select>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <div class=" col-xs-offset-4 col-sm-offset-4 col-md-offset-2 col-lg-offset-2 col-sm-10">
+                            <button type="submit" name="creeer" class="btn btn-primary"> Opslaan </button>
+                        </div>
+                    </div>
+                 </form>
+    <!--                            <a href="Overzicht.php"><span style="font-size:3.0em;" class="glyphicon glyphicon-circle-arrow-left"></span></a>-->
+                 <div class="text-center col-sm-12 col-md-12 col-lg-12" id="back">
+                     <a href="Overzicht.php">Terug naar het overzicht</a>
+                 </div>
+          </div>
         </div>
     </div>
-</div>
-<script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        if ($("#side-toggle").hasClass("glyphicon-option-vertical")) {
-            $("#side-toggle").removeClass("glyphicon-option-vertical");
-            $("#side-toggle").addClass("glyphicon-option-horizontal");
-        } else {
-            $("#side-toggle").removeClass("glyphicon-option-horizontal");
-            $("#side-toggle").addClass("glyphicon-option-vertical");
-        }
-    });
-</script>
-</body>
-</html>
-
+    <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+            if ($("#side-toggle").hasClass("glyphicon-option-vertical")) {
+                $("#side-toggle").removeClass("glyphicon-option-vertical");
+                $("#side-toggle").addClass("glyphicon-option-horizontal");
+            } else {
+                $("#side-toggle").removeClass("glyphicon-option-horizontal");
+                $("#side-toggle").addClass("glyphicon-option-vertical");
+            }
+        });
+    </script>
+    </body>
+    </html>
+<?php }
+?>
