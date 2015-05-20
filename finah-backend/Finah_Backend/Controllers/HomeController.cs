2deb5ff76c8189172e3b1773951c9fd897f7c28b
Brian Thys <brianthys@hotmail.com>
2015-05-20 19:44:52 +0200

    [Backend] lol...
    I idioto :D

--- finah-backend/Finah_Backend/Controllers/HomeController.cs
+++ finah-backend/Finah_Backend/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ namespace Finah_Backend.Controllers
 {
     public class HomeController : Controller
     {
-        [Authorize]
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
