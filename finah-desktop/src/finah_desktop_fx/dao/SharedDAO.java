a625a38de91993891e1b550923b64351c5f16deb
Sam Wuytens <sam.wuytens@student.pxl.be>
2015-05-22 15:24:39 +0200

    [Desktop] Post Method aan proberen te maken

--- finah-desktop/src/finah_desktop_fx/dao/SharedDAO.java
+++ finah-desktop/src/finah_desktop_fx/dao/SharedDAO.java
@@ -10,6 +10,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.HttpContext;
 
@@ -22,25 +23,37 @@ public class SharedDAO {
 			throws ClientProtocolException, IOException {
 
 		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-		request.setHeader("Authorization", "Bearer ZfmgrSW85TXRy9V_xQ7t7NIRiydl0iITdYCcbCTfLwFuWYbphfEy7oCgBOTSvmRLYBR0ZQ9IzW2Ot7Ayln3UXpez1ZDEX8v5N1Y3uF_HXddnvr-5xPTa0W1B9_dDfqHIqwI8UJruM-ZkrssERWFuAPypztcPsj3vsB0DOtz63u1npCqWGt5mJj6HBoYf8WL9wt70ndzXvde6qDZ54yTQRs1tEWC0ZzteAZjhrVp1KHOyDhzXB70mjbMWJWIYLGiDghp9iTINV4LtI-CuH7Gd1t504Z7TPF1wRrDuNznMcKLurcoMktPxpAsVzWNy1prlfhumpMFRu8zTww0lqVjjXOJvXNIhDORnPd1ITwRPS-WG8sJrsFDeU_W7nhFcycLSM4EtkOpAaRcidhqXUL_WlEsgu_9Sq8-bNNPVSxto0KCo0Eb04JXppLCp-eR2rbYc-mV_RIcIqRKNU4zWO3k4voxH6UCoiigtTDY24NpD6mpiII59ZvwEsNaCsYplZY8EyWry26ZfL8uk3-buoOgoMg");
-		HttpResponse response = client.execute(request);
+		HttpGet get = new HttpGet(url);
+		get.setHeader("Authorization", "Bearer ZfmgrSW85TXRy9V_xQ7t7NIRiydl0iITdYCcbCTfLwFuWYbphfEy7oCgBOTSvmRLYBR0ZQ9IzW2Ot7Ayln3UXpez1ZDEX8v5N1Y3uF_HXddnvr-5xPTa0W1B9_dDfqHIqwI8UJruM-ZkrssERWFuAPypztcPsj3vsB0DOtz63u1npCqWGt5mJj6HBoYf8WL9wt70ndzXvde6qDZ54yTQRs1tEWC0ZzteAZjhrVp1KHOyDhzXB70mjbMWJWIYLGiDghp9iTINV4LtI-CuH7Gd1t504Z7TPF1wRrDuNznMcKLurcoMktPxpAsVzWNy1prlfhumpMFRu8zTww0lqVjjXOJvXNIhDORnPd1ITwRPS-WG8sJrsFDeU_W7nhFcycLSM4EtkOpAaRcidhqXUL_WlEsgu_9Sq8-bNNPVSxto0KCo0Eb04JXppLCp-eR2rbYc-mV_RIcIqRKNU4zWO3k4voxH6UCoiigtTDY24NpD6mpiII59ZvwEsNaCsYplZY8EyWry26ZfL8uk3-buoOgoMg");
+		HttpResponse response = client.execute(get);
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response
 				.getEntity().getContent()));
 
 		return rd;
 	}
+	
 	public static <T> ArrayList<T> HaalGegevens(String url,Type typ)
 			throws ClientProtocolException, IOException {
 		Gson gson = new GsonBuilder().serializeNulls().create();
 		HttpClient client = new DefaultHttpClient();
-		HttpGet request = new HttpGet(url);
-		request.setHeader("Authorization", "Bearer ZfmgrSW85TXRy9V_xQ7t7NIRiydl0iITdYCcbCTfLwFuWYbphfEy7oCgBOTSvmRLYBR0ZQ9IzW2Ot7Ayln3UXpez1ZDEX8v5N1Y3uF_HXddnvr-5xPTa0W1B9_dDfqHIqwI8UJruM-ZkrssERWFuAPypztcPsj3vsB0DOtz63u1npCqWGt5mJj6HBoYf8WL9wt70ndzXvde6qDZ54yTQRs1tEWC0ZzteAZjhrVp1KHOyDhzXB70mjbMWJWIYLGiDghp9iTINV4LtI-CuH7Gd1t504Z7TPF1wRrDuNznMcKLurcoMktPxpAsVzWNy1prlfhumpMFRu8zTww0lqVjjXOJvXNIhDORnPd1ITwRPS-WG8sJrsFDeU_W7nhFcycLSM4EtkOpAaRcidhqXUL_WlEsgu_9Sq8-bNNPVSxto0KCo0Eb04JXppLCp-eR2rbYc-mV_RIcIqRKNU4zWO3k4voxH6UCoiigtTDY24NpD6mpiII59ZvwEsNaCsYplZY8EyWry26ZfL8uk3-buoOgoMg");
-		HttpResponse response = client.execute(request);		
+		HttpGet get = new HttpGet(url);
+		get.setHeader("Authorization", "Bearer ZfmgrSW85TXRy9V_xQ7t7NIRiydl0iITdYCcbCTfLwFuWYbphfEy7oCgBOTSvmRLYBR0ZQ9IzW2Ot7Ayln3UXpez1ZDEX8v5N1Y3uF_HXddnvr-5xPTa0W1B9_dDfqHIqwI8UJruM-ZkrssERWFuAPypztcPsj3vsB0DOtz63u1npCqWGt5mJj6HBoYf8WL9wt70ndzXvde6qDZ54yTQRs1tEWC0ZzteAZjhrVp1KHOyDhzXB70mjbMWJWIYLGiDghp9iTINV4LtI-CuH7Gd1t504Z7TPF1wRrDuNznMcKLurcoMktPxpAsVzWNy1prlfhumpMFRu8zTww0lqVjjXOJvXNIhDORnPd1ITwRPS-WG8sJrsFDeU_W7nhFcycLSM4EtkOpAaRcidhqXUL_WlEsgu_9Sq8-bNNPVSxto0KCo0Eb04JXppLCp-eR2rbYc-mV_RIcIqRKNU4zWO3k4voxH6UCoiigtTDY24NpD6mpiII59ZvwEsNaCsYplZY8EyWry26ZfL8uk3-buoOgoMg");
+		HttpResponse response = client.execute(get);		
 		BufferedReader rd = new BufferedReader(new InputStreamReader(response
 				.getEntity().getContent()));
 
 		ArrayList<T> lijst = gson.fromJson(rd, typ);
 		return lijst;
+	}
+	
+	//Werkt niet normaal
+	public static BufferedReader StuurGegevens(String url) throws ClientProtocolException, IOException {
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(url);
+		post.setHeader("Authorization", "Bearer ZfmgrSW85TXRy9V_xQ7t7NIRiydl0iITdYCcbCTfLwFuWYbphfEy7oCgBOTSvmRLYBR0ZQ9IzW2Ot7Ayln3UXpez1ZDEX8v5N1Y3uF_HXddnvr-5xPTa0W1B9_dDfqHIqwI8UJruM-ZkrssERWFuAPypztcPsj3vsB0DOtz63u1npCqWGt5mJj6HBoYf8WL9wt70ndzXvde6qDZ54yTQRs1tEWC0ZzteAZjhrVp1KHOyDhzXB70mjbMWJWIYLGiDghp9iTINV4LtI-CuH7Gd1t504Z7TPF1wRrDuNznMcKLurcoMktPxpAsVzWNy1prlfhumpMFRu8zTww0lqVjjXOJvXNIhDORnPd1ITwRPS-WG8sJrsFDeU_W7nhFcycLSM4EtkOpAaRcidhqXUL_WlEsgu_9Sq8-bNNPVSxto0KCo0Eb04JXppLCp-eR2rbYc-mV_RIcIqRKNU4zWO3k4voxH6UCoiigtTDY24NpD6mpiII59ZvwEsNaCsYplZY8EyWry26ZfL8uk3-buoOgoMg");
+		HttpResponse response = client.execute(post);
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		
+		return rd;
 	}
 }
